6980b643501f55e40230c265252948d98988d1ec
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-07-28 15:53:57 -0300

    Adicionado exercicio 3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-3.js
@@ -0,0 +1,12 @@
+var notaCandidato = 100;
+
+if (notaCandidato >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (notaCandidato < 80 && notaCandidato >= 60) {
+   console.log("Você está em nossa lista de espera");
+}
+
+else {
+   console.log("Você foi reprovada(o)");
+}
