f3349de793f1d4049da13387e7ba6f016501044d
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-07-28 18:07:32 -0300

    Adicionado exercicios 1,2 e 3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-1.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 2;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(a);
+console.log(b);
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
