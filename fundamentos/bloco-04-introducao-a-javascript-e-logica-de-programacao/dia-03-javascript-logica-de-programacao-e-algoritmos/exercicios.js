31bc5831b710fd40a2196452fe71461e9968a70d
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-07-30 19:04:22 -0300

    Adicionado exercicio 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -2,11 +2,39 @@
 
 let n = 5;
 
-for (index = 0; index < n; index += 1) {
+
+console.log("Exercicio 1");
+console.log(" ");
+
+for (let index = 0; index < n; index += 1) {
   let imp = " ";
   for (let linha = 0; linha < n; linha += 1) {
     imp = imp + "*";
   }
 
   console.log(imp);
+ }
+
+console.log(" ");
+console.log("Exercicio 2");
+console.log(" ");
+
+
+
+for (let index = 0; index < n; index += 1) {
+  let imp = " ";
+  
+  if (index < 5) {
+    
+    for (linha = 0; linha < index + 1; linha += 1) {
+    imp = imp + "*";
+    }
+  console.log(imp);
+  
+  }
+
+  else {
+    console.log("erro");
+  }
 }
+
