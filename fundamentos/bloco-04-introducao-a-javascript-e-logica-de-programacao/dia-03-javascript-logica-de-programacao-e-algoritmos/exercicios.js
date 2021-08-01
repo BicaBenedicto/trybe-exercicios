5122c8f8060e8ba91082235f3acdedf92cf31d1f
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-01 01:44:54 -0300

    Adicionado Exercicio Bonus 5

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,16 +1,16 @@
 //Exercicio 1
 
-let n = 5;
+let n = 7;
 
 
 console.log("Exercicio 1");
 console.log(" ");
 
 for (let index = 0; index < n; index += 1) {
-  let imp = " ";
+  let imp = "";
 
   for (let linha = 0; linha < n; linha += 1) {
-    imp = imp + "*";
+    imp += "*";
   }
 
   console.log(imp);
@@ -23,13 +23,14 @@ console.log(" ");
 
 
 for (let index = 0; index < n; index += 1) {
-  let imp = " ";
+  let imp = "";
   
       for (let linha = 0; linha <= index; linha += 1) {
     imp = imp + "*";
+    
     }
+
   console.log(imp);
-  
 }
 
 console.log(" ");
@@ -37,7 +38,7 @@ console.log("Exercicio 3");
 console.log(" ");
 
 for (let index = 0; index < n; index += 1) {
-  let imp = " ";
+  let imp = "";
 
      for (let linha = n - 1; linha > index; linha -= 1) {
         imp = imp + " ";
@@ -63,16 +64,51 @@ for (let index = 0; index < n; index += 1) {
       imp = imp + " ";
     }
 
-     for (let linha = 0; linha <= index; linha += 1) {
+    for (let linha = 0; linha <= index; linha += 1) {
       imp = imp + "*";
     }
 
-    if (index > 0 && index % 2 > 0) {
-      imp = imp + "*"
-    }
-    
+
     console.log(imp);
   }
-  
+}
+
+console.log(" ");
+console.log("Bonus");
+console.log("Exercicio 5");
+console.log(" ");
+
+
+
+for (let index = 0; index < n; index += 1) {
+  let pyramid = "";
+    if  (index < n - 1 && index % 2 === 0) {
+      
+      for(let line = n - 2; line > index; line -= 2){
+        pyramid += " ";
+      }
+
+      for (let linha = 0; linha <= index; linha += 1) {
+        if (linha === 0 || linha === index) {
+          pyramid += "*";
+        }
+
+        else if (linha > 0 && linha < index) {
+          pyramid += " ";
+        
+        }
+      
+      }
+
+    console.log(pyramid);
+  }
+  else if (index === n - 1) {
+
+    for (let linha = 0; linha <= index; linha += 1) {
+      pyramid += "*";
+    }
+
+    console.log(pyramid);
+  }
   
 }
