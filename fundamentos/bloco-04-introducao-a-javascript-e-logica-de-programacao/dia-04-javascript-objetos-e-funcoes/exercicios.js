f67750ca75a4f7702e6777a65fc045a5092b0f4d
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-02 04:38:12 -0300

    Adicionado exercicio 5 de objetos

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios.js
@@ -24,3 +24,7 @@ player["bestInTheWorld"] = [2006, 2007, 2007, 2009, 2010, 2018]
 player ["fullName"] = player.name + " " + player.lastName;
 
 console.log("A jogadora " + player.fullName + " foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " vezes");
+
+//Exercicio 5
+
+console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata.");
