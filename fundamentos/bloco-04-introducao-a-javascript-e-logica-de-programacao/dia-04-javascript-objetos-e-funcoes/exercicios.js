2b9ce57135f1351160c92ab81bb11271708778c0
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-02 13:39:47 -0300

    Adicionado exercicio 6 de for/in

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios.js
@@ -1,4 +1,4 @@
-//Exercicio 1
+//Exercicio 1 - Objeto
 
 let player = {
   name: "Marta",
@@ -10,21 +10,34 @@ let player = {
   }
 }
 
-//Exercicio 2
+//Exercicio 2 - Objeto
 
 console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade");
 
 
-//Exercicio 3
+//Exercicio 3 - Objeto
 
 player["bestInTheWorld"] = [2006, 2007, 2007, 2009, 2010, 2018]
 
-//Exercicio 4
+//Exercicio 4 - Objeto
 
 player ["fullName"] = player.name + " " + player.lastName;
 
 console.log("A jogadora " + player.fullName + " foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " vezes");
 
-//Exercicio 5
+//Exercicio 5 - Objeto
 
 console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata.");
+
+
+//Exercicio 6 - For/in
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
+
+for (let show in names) {
+  console.log("Olá " + names[show]);
+}
