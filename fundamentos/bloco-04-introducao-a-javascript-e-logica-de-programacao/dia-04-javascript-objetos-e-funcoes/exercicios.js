ac8f98b74188ad5845aab410850a060983bbeb86
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-02 04:22:14 -0300

    Adicionado exercicio 2 de objetos

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios.js
@@ -1,11 +1,16 @@
+//Exercicio 1
+
 let player = {
   name: "Marta",
   lastName: "Silva",
-  age: 24,
+  age: 34,
   medals: {
     golden: 2,
     silver: 3
   }
 }
 
-console.table(player);
+//Exercicio 2
+
+console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade");
+
