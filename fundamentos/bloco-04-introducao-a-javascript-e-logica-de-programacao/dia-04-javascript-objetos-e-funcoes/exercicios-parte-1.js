150f6ba0c03c207f872af6c1299618bc8208fc42
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-03 02:52:33 -0300

    Adicionado exercicio 4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-parte-1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-parte-1.js
@@ -8,14 +8,23 @@ let info = {
 
 console.log("Bem-vinda, " + info.personagem);
 
+console.log(" ");
 //Exercicio 2
 
 info ["recorrente"] = "Sim";
 
 console.log(info);
 
+console.log(" ");
 //Exercicio 3
 
 for (let show in info) {
   console.log(show);
+}
+
+console.log(" ");
+//Exercicio 4
+
+for (let show in info) {
+  console.log (info[show]);
 }
