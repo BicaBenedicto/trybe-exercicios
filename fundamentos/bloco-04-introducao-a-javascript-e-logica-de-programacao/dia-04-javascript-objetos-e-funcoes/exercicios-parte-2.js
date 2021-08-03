39f8904a3f93a318c2331eaceec6c0a5d1ae4c00
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-03 04:31:34 -0300

    Adicionado exercicio 1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-parte-2.js
@@ -0,0 +1,15 @@
+function palindromo (palavra) {
+  let palindromo = "";
+
+for(let index = palavra.length - 1; index >= 0; index -= 1) {
+  palindromo += palavra[index]
+}
+
+if (palavra === palindromo) {
+  return true;
+} else {
+  return false;
+}
+}
+
+console.log(palindromo("arara"));
