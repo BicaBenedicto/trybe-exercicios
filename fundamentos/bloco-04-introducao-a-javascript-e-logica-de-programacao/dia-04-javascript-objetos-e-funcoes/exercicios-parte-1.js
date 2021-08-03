be8e96c80855283774cb622a1055b7efadd6f2c2
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-03 03:46:04 -0300

    Adicionado exercicio 5

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-parte-1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-parte-1.js
@@ -1,7 +1,7 @@
 let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 
 //Exercicio 1
@@ -28,3 +28,26 @@ console.log(" ");
 for (let show in info) {
   console.log (info[show]);
 }
+
+console.log(" ");
+//Exercicio 5
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim"
+}
+
+if (info.recorrente === "Sim" && info2.recorrente === "Sim") {
+  info2.recorrente = "Sim";
+  info.recorrente = "Ambos recorrentes, Sim";
+  
+  for (let show in info) {
+    console.log(info[show] + " e " + info2[show]);
+  }
+  
+}
+
+      
+
