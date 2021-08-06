7a92e9b1a119744d6aa5d97529e22afd3c2fa010
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-06 02:00:07 -0300

    Adicionado exercicios

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-parte-2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-parte-2.js
@@ -76,6 +76,43 @@ function maiorRepeticao (valores) {
 
 maiorRepeticao([2, 3, 2, 5, 8, 2, 3]);
 
+let array = [0, 4, 4, 4, 9, 2, 1];
+
+function moreFrequentNumber(array) {
+  let numbers = [];
+
+  for (let index of array) {
+    cont = checkNumber(numbers, index);
+   if (cont === 0) {
+      numbers.push(index);
+    }
+  }
+  return checkRepeated(numbers, array);
+}
+function checkNumber(array, n) {
+ let cont = 0;
+  for (let number of array) {
+    if (n === number) {
+      cont += 1;
+    }
+  }
+  return cont;
+}
+function checkRepeated(numerosParaChecar, vetorDeNumerosOriginal) {
+  let moreFrequent = 0;
+  let moreFrequentRep = 0;
+  for (let i in numerosParaChecar) {
+    let rep = checkNumber(vetorDeNumerosOriginal, numerosParaChecar[i]);
+    if (rep > moreFrequentRep) {
+      moreFrequent = numerosParaChecar[i];
+      moreFrequentRep = rep;
+    }
+  }
+  return moreFrequent;
+}
+
+console.log(moreFrequentNumber(array));
+
 
 //Exercicio 6
 
