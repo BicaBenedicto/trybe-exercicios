12197eba9d7bdbd13c362358eb1a94e0d426f7d1
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-08 01:14:42 -0300

    Realizado exercicio bonus 3'

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-bonus-3.js
@@ -0,0 +1,45 @@
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+function cestaDeCompras (cesta) {
+  cesta = basket;
+  let cestaItems = {};
+  let output2 = [];
+  let output = "Sua cesta possui: ";
+
+  for (let key of cesta){
+    if (cestaItems[key]) {
+      cestaItems[key] += 1;
+    } else {
+      cestaItems[key] = 1;
+    }
+  }
+  for (let i in cestaItems) {
+    if (cestaItems[i]) {
+      output2.push(cestaItems[i]);
+      output2.push(' ');
+      output2.push([i]);
+      output2.push('s, ');
+    }
+  }
+  return output + output2.join('');
+}
+
+console.log(cestaDeCompras());
