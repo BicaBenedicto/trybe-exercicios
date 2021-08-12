e2476255718c2911829f90448de5e95f97fb7486
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-11 22:29:06 -0300

    Realizado exercicio 2

--- fundamentos/bloco-05-javascripit-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicio-principal/script.js
+++ fundamentos/bloco-05-javascripit-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicio-principal/script.js
@@ -16,13 +16,13 @@ createDaysOfTheWeek();
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 
-function holiday (day) {
+function checkHoliday (day) {
   if (day === 24 || day === 25 || day === 31) {
     return true;
   }
 }
 
-function friday (day) {
+function checkFriday (day) {
   if (day === 4 || day === 11 || day === 18 || day === 25) {
     return true;
   }
@@ -32,15 +32,26 @@ function calendarDays () {
     let days = document.querySelector('#days');
     let dayN = document.createElement('li');
     dayN.innerText = i;
-    dayN.className = "day";
-    if (holiday(i)) {
-      dayN.classList.add("holiday");
+    dayN.className = 'day';
+    if (checkHoliday(i)) {
+      dayN.classList.add('holiday');
     };
-    if (friday(i)) {
-      dayN.classList.add("friday");
+    if (checkFriday(i)) {
+      dayN.classList.add('friday');
     };
     days.appendChild(dayN);
   }
 }
 
 calendarDays();
+
+function holidayButton (Feriados) {
+  let buttonFeriado = document.createElement('button');
+  buttonFeriado.innerText = 'Feriados';
+  buttonFeriado.id = 'btn-holiday';
+
+  let buttonsContainer = document.querySelector('div.buttons-container');
+  buttonsContainer.appendChild(buttonFeriado);
+}
+
+holidayButton();
