0a825144f460010bcda064f67c14cf69c53a90c2
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-19 03:51:16 -0300

    Realizado exercicio 2 de pratica

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercicio-pratica/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercicio-pratica/script.js
@@ -41,3 +41,73 @@ function selectStates() {
 }
 
 selectStates();
+
+let dateError = [];
+
+function checkDay(day) {
+  const dayString = day[0] + day[1];
+  const dayNumber = parseInt(dayString);
+  let output = false;
+
+  if (dayNumber > 0 && dayNumber < 32) {
+    output = true;
+  } if (!output) {
+  dateError.push('Dia invalido');
+  }
+  return output;
+}
+
+function checkMonth(month) {
+  const monthString = month[3] + month[4];
+  const monthNumber = parseInt(monthString);
+  let output = false;
+
+  if (monthNumber > 0 && monthNumber <= 12) {
+    output = true;
+  } if (!output) {
+  dateError.push('Mês invalido');
+  }
+  return output;
+}
+function checkYear(year) {
+  const yearString = year[6] + year[7] + year[8] + year[9];
+  const yearNumber = parseInt(yearString);
+  let output = false;
+
+  if (yearNumber > 0) {
+    output = true;
+  } if (!output) {
+  dateError.push('Ano invalido');
+  }
+  return output;
+}
+
+function checkDateFormat(date) {
+  const dateString = date[2] + date[5];
+  let output = false;
+
+  if (dateString.includes('//')) {
+    output = true;
+  } if (!output) {
+  dateError.push('Formato invalido');
+  }
+  return output;
+}
+
+function compareCheckDate(date) {
+  return checkDay(date) && checkMonth(date) && checkYear(date) && checkDateFormat(date);
+}
+
+function checkDate() {
+  const date = document.getElementById('date');
+  
+  if(!compareCheckDate(date.value)) {
+    alert(dateError);
+    date.value = '';
+    dateError = [];
+  }
+}
+
+const outDate = document.getElementById('date');
+
+outDate.addEventListener('change', checkDate);
