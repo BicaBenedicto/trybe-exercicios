3c4d5d601390cedbe6f405b0fd29488dba4f95ca
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-19 02:43:39 -0300

    Realizado exercicio 1 de pratica

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercicio-pratica/script.js
@@ -0,0 +1,43 @@
+states = {
+  'AC': 'Acre' ,
+  'AL': 'Alagoas' ,
+  'AP': 'Amapá' ,
+  'AM': 'Amazonas' ,
+  'BA': 'Bahia' ,
+  'CE': 'Ceará' ,
+  'DF': 'Distrito Federal' ,
+  'ES': 'Espírito Santo' ,
+  'GO': 'Goías' ,
+  'MA': 'Maranhão' ,
+  'MT': 'Mato Grosso' ,
+  'MS': 'Mato Grosso do Sul' ,
+  'MG': 'Minas Gerais' ,
+  'PA': 'Pará' ,
+  'PB': 'Paraíba' ,
+  'PR': 'Paraná' ,
+  'PE': 'Pernambuco' ,
+  'PI': 'Piauí' ,
+  'RJ': 'Rio de Janeiro' ,
+  'RN': 'Rio Grande do Norte' ,
+  'RS': 'Rio Grande do Sul' ,
+  'RO': 'Rondônia' ,
+  'RR': 'Roraíma' ,
+  'SC': 'Santa Catarina' ,
+  'SP': 'São Paulo' ,
+  'SE': 'Sergipe' ,
+  'TO': 'Tocantins' ,
+};
+
+function selectStates() {
+  const select = document.getElementById('estado');
+
+  for(let index in states) {
+    const state = document.createElement('option');
+
+    state.innerText = states[index];
+    state.value = index;
+    select.appendChild(state);
+  }
+}
+
+selectStates();
