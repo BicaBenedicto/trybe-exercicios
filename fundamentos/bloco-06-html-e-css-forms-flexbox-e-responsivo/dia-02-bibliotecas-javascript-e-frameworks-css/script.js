38789bd9a2be48947ab6e1dbd40e2f5b405b1250
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-19 21:51:28 -0300

    Realizado exercicio 2

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
@@ -42,74 +42,6 @@ function selectStates() {
 
 selectStates();
 
-let dateError = [];
-
-function checkDay(day) {
-  const dayString = day[0] + day[1];
-  const dayNumber = parseInt(dayString);
-  let output = false;
-
-  if (dayNumber > 0 && dayNumber < 32) {
-    output = true;
-  } if (!output) {
-  dateError.push('Dia invalido');
-  }
-  return output;
-}
-
-function checkMonth(month) {
-  const monthString = month[3] + month[4];
-  const monthNumber = parseInt(monthString);
-  let output = false;
-
-  if (monthNumber > 0 && monthNumber <= 12) {
-    output = true;
-  } if (!output) {
-  dateError.push('Mês invalido');
-  }
-  return output;
-}
-function checkYear(year) {
-  const yearString = year[6] + year[7] + year[8] + year[9];
-  const yearNumber = parseInt(yearString);
-  let output = false;
-
-  if (yearNumber > 0) {
-    output = true;
-  } if (!output) {
-  dateError.push('Ano invalido');
-  }
-  return output;
-}
-
-function checkDateFormat(date) {
-  const dateString = date[2] + date[5];
-  let output = false;
-
-  if (dateString.includes('//')) {
-    output = true;
-  } if (!output) {
-  dateError.push('Formato invalido');
-  }
-  return output;
-}
-
-function compareCheckDate(date) {
-  return checkDay(date) && checkMonth(date) && checkYear(date) && checkDateFormat(date);
-}
-
-function checkDate() {
-  const date = document.getElementById('date');
-  
-  if(!compareCheckDate(date.value)) {
-    alert(dateError);
-    date.value = '';
-    dateError = [];
-  }
-}
-
-const outDate = document.getElementById('date');
-outDate.addEventListener('change', checkDate);
 
 function checkFormValue() {
   const formReply = {
@@ -122,7 +54,7 @@ function checkFormValue() {
     'Resumo currículo: ': document.getElementById('resume-curriculum').value,
     'Cargo: '           : document.getElementById('cargo').value,
     'Descrição cargo: ' : document.getElementById('desc-cargo').value,
-    'Data de inicio: '  : document.getElementById('date').value,
+    'Data de inicio: '  : document.getElementById('datepicker').value,
   };
   let output = 0;
   
@@ -154,7 +86,7 @@ function sendSections(block) {
     'Resumo currículo: ': document.getElementById('resume-curriculum').value,
     'Cargo: '           : document.getElementById('cargo').value,
     'Descrição cargo: ' : document.getElementById('desc-cargo').value,
-    'Data de inicio: '  : document.getElementById('date').value,
+    'Data de inicio: '  : document.getElementById('datepicker').value,
   };
 
   for (let item in formReply) {
