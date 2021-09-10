cb15e0153dc0053dbda2c6b35c2eb7a7db67ab8a
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-09-09 22:04:16 -0300

    Exercicio 6

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercise6.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+  return students.map((name, index, arr) => arr.reduce((results) => {
+    results.name = name;
+    results.average = grades[index].reduce((result, values) => result + values, 0) / grades[index].length;
+    return results;
+    }
+  ,{}));
+};
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepStrictEqual(studentAverage(), expected);
